bea48a8fe4f9c6f6934c10ed945588f5cf830244
tr0cse <gurkanakyuz75@gmail.com>
2021-09-18 21:06:12 +0300

    Creating form for adding topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import render, redirect
+
 from .models import Topic
-from django.shortcuts import render
+from .forms import TopicForm
+
 
 def index(request):
     """The home page for Learning Log."""
@@ -17,3 +20,17 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by("-date_added")
     context = {'topic' : topic, 'entries' : entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != "POST":
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topics")
+        
+    context = {"form" : form}
+    return render(request, "learning_logs/new_topic.html", context)
